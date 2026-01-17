2861bdfb763ac39c1b4854ded17477c9cac868c1
cyrillweber <cyrill.weber1@edu.tbz.ch>
2026-01-17 14:00:20 +0100

    update cloudflare config

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare'; 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter({
-			config: './wrangler.jsonc',
-			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
-			},
-			fallback: 'plaintext',
 			routes: {
 				include: ['/*'],
 				exclude: ['<all>']
-			}
+			},
+            platformProxy: {
+                configPath: './wrangler.jsonc'
+            }
 		})
 	}
 };
